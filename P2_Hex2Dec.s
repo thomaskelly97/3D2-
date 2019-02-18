90b040677a9cc7932d9917d9fffa87cfb47d3f1c
Thomas Kelly <tkelly2@tcd.ie>
2019-02-18 10:00:39 +0000

    Nearly complete number calculation

--- P2_Hex2Dec.s
+++ P2_Hex2Dec.s
@@ -2,18 +2,68 @@
 	IMPORT	main
 	EXPORT	start
 start 
-	LDR	r0, =test ;INPUT 
+	LDR r0, =test ;INPUT 
 	LDR r1, [r0]
-	LDR r2, =0 
-	LDR r3, =0xA1001004 ;STACK 
-	LDR r4, =8
+	LDR r2, =8
+	LDR r3, =0 ;STACK 
+	LDR r4, =0
+	LDR r5, =0 
 	LDR r6, =0
 	LDR r7, =10000
 	LDR r8, =1000
 	LDR r9, =100 
-	LDR r10,=10 ;DEFINE ALL DECADES 
+	LDR r10,=8 ;Just need it for a sec, redefined near line 35
 	LDR r11,=1 
-
+	LDR r12,=0xF
+	
+	MOV r5,r1 
+	LDR r6,=2_1111 
+	
+	;at this point, r3,r5 can be used. Maybe r6? 
+	;THIS PART COUNTS THE LENGTH OF THE INPUT. 
+initl 
+	MOV r5,r5,ROR #28 ;Get the bits in order 
+	AND r3,r5, r6 ;get the first nibble 
+	CMP r3, #0
+	BNE skip000
+	ADD r4,r4,#1 ;Count the amount of zero nibbles there is 
+	SUB r2,r2,#1
+	CMP r2,#0
+	BNE initl
+skip000
+	;NOW r4, has the amount of '0000' periods before we hit a nonzero 
+	SUB r4,r10,r4 ;Now it has the amount of F periods we need 
+	SUB r4,r4,#1 ;CORRECT 
+	
+	;THIS PART GENERATES F's the SAME LENGTH AS INPUT 
+	ORR r10,r12,#0
+Floop 
+	MOV r12,r12,LSL #4 ;Move the F out left 'r4' times  
+	ORR r10,r12,r10 
+	SUB r4,r4,#1 ;Countdown 
+	CMP r4,#0 
+	BNE Floop ; now F is over 'r4' positions to the left, we have the F's we need 
+	;r1 = 0xFFF - 0x419 etc. 
+	 
+	
+	;DETERMINE WHICH SIGN. USE r3 =0 for POSITIVE, r3 = 1 for NEGATIVE 
+	MOV r3,r1 ;make a copy 
+	MOV r3,r3,LSR #31 ;****THE PROBLEM IS HERE, WE AREN'T CHECKING THE FIRST BIT**** 
+	AND r3,r3,r11 ;AND with 0001 
+	
+	CMP r3,#0 ;If r3 is 0 then the number is positive 
+	BEQ skip00 ;if r3 == 0 skip this
+	MOV r3, #1 ;Put 1 in r3 to signify negative number NEGATIVE 
+	SUB r1,r10,r1 ;Take away using previously calculated shite 
+skip00  
+	CMP r3, #1 ;If r3 == 1 then skip this
+	BEQ skip01
+	MOV r3,#0 ;POSTIVIE 
+skip01
+	
+	;CALCULATE THE NUMBER HEX -> DECIMAL 
+	LDR r10,=10
+	LDR r2,=0 
 loop
 	CMP r7,r1 ;If r7 > r1 we will get negative so skip this 
 	BGT skip0
@@ -21,9 +71,9 @@ loop
 	ADD r2,r2,#1 
 	B loop ;keep looping 
 skip0
-	;PUSH 
-	SUB r3,r3,#4
-	STR r2, [r3] 
+	;PUSH 01 
+	SUB r13,r13,#4
+	STR r2, [r13] 
 	
 	MOV r2,#0
 loop1
@@ -33,9 +83,9 @@ loop1
 	ADD r2,r2,#1 
 	B loop1 ;keep looping 
 skip1 
-	;PUSH
+	;PUSH 02
 	 
-	STR r2, [r3, #-4]! 
+	STR r2, [r13, #-4]! 
 	
 	MOV r2,#0
 loop2
@@ -45,9 +95,9 @@ loop2
 	ADD r2,r2,#1 
 	B loop2 ;keep looping 
 skip2
-	;PUSH 
+	;PUSH 03
 	
-	STR r2, [r3, #-4]!
+	STR r2, [r13, #-4]!
 	
 	MOV r2,#0
 loop3
@@ -57,9 +107,8 @@ loop3
 	ADD r2,r2,#1 
 	B loop3 ;keep looping 
 skip3
-	;PUSH
-	
-	STR r2, [r3, #-4]!
+	;PUSH 04
+	STR r2, [r13, #-4]!
 	
 	MOV r2,#0
 loop4
@@ -69,18 +118,18 @@ loop4
 	ADD r2,r2,#1 
 	B loop4 ;keep looping 
 skip4
-	 ;PUSH 
-	STR r2, [r3, #-4]!
-	LDR r5, =0xA1001004
-	MOV r3,r5
+	 ;PUSH 05
+	STR r2, [r13, #-4]!
+	ADD r13,r13,#20 ;Reset the stack pointer 
 	MOV r2,#0
-	 	 
+	
+	LDR r4, =8  
 loop5 
-	LDR r6, [r3],#-4
+	LDR r6, [r13],#-4 ;Loop down through the number in order 
 	SUB r4,r4,#1 
 	CMP r4,#0 
 	BNE loop5
 	
 stop	B	stop
-test	DCD 0x419,0
+test	DCD 0xFFE4,0
 	END
